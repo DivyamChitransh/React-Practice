2a6bb0483327c91ba737268c387815a81e08defa
Divyam Chitransh <divyam@apeiros.com>
2026-08-18 13:30:00 +0530

    Updates in form-data example

--- form-data/src/App.jsx
+++ form-data/src/App.jsx
@@ -1,31 +1,37 @@
 import { useState } from "react";
+import "./App.css";
 
-function App(){
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [isLogin,setIslogin] = useState(false);
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setIslogin(true)
-  }
-  return ( 
-    <div>
-      <h2>Login Form:</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email"/>
-        </div>
-        <div>
-        <label>Password:</label>
-        <input type="password" vlaue={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password"/>
-        </div>
-      <button type="submit">Submit</button>
-      </form>
-      {isLogin && (<h2>Logged in with : {email}</h2>)}
+    setIsLogin(true);
+  };
 
+  return (
+    <div className="page">
+      <div className="login-card">
+        <h1>Login Form</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email"/>
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password"/>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        {isLogin && (
+          <h2 className="success">Login Successful</h2>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
