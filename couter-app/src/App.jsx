62bf18ab9355a8bd45c5cbaf3742f327935151ec
Divyam Chitransh <divyam@apeiros.com>
2026-08-19 09:51:35 +0530

    Q1 Counter App

---
+++ couter-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function Counter(){
+  const [count,setCount] = useState(0);
+  const increment = () => {
+    setCount(count+1)
+  }
+  const decrement = () => {
+    setCount(count -1);
+  }
+  const reset = () => {
+    setCount(0)
+  }
+  return (
+    <div>
+      <div>
+        <h2>Count: {count}</h2>
+      </div>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>0</button>
+    </div>
+  )
+}
+
+export default Counter;
