c0cbf9fd38f17d7f00e7cc8f30409ff6a8aec355
Divyam Chitransh <divyam@apeiros.com>
2026-08-20 17:10:25 +0530

    Q13 Search filter

---
+++ Q13-Search-filter/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function App(){
+  const [search,setSearch] = useState("")
+
+  const items = ["Apple","Mango","Banana","Guava","Pineapple"];
+
+  const filteredItems = search === "" ? [] : items.filter((item) => item.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+
+  return (
+    <div>
+      <h2>Search Filter</h2>
+      <div>
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search..."/>
+        <ul>{filteredItems.map((item,index) => (
+          <li key={index}>{item}</li>
+        ))}</ul>
+      </div>
+    </div>
+  )
+}
+
+export default App;
