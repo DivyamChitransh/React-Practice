419d4dde587ac68f97b373c495eb4bbd21c9479b
Divyam Chitransh <divyam@apeiros.com>
2026-08-22 09:33:52 +0530

    React Router Explanation

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,36 @@
+
+import { Route,Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Product from "./components/Products";
+
+function Home(){
+  return <h1>Home Page</h1>
+}
+
+function About(){
+  return <h1>About Page</h1>
+}
+
+function Contact(){
+  return <h1>Contacts Page</h1>
+}
+
+function App(){
+  return (
+    <>
+      <Navbar />
+    <Routes>
+      <Route path = "/" element = {<Home />}></Route>
+      <Route path = "/about" element = {<About />}></Route>
+      <Route path = "/contact" element = {<Contact />}></Route>
+      <Route path = "/product" element = {<Product />}></Route>
+      <Route path = "/login" element = {<Login />}></Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App;
+
+
